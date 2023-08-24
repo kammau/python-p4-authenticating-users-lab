80e9a4afbc0471c4de86f231442b909ba0a58bf3
Kamryn Smith <programersign@gmail.com>
2023-08-24 13:52:29 -0700

    assignment complete

--- server/app.py
+++ server/app.py
@@ -52,6 +52,36 @@ api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
+class Login(Resource):
+    def post(self):
+        username = request.get_json()["username"]
+        user = User.query.filter(User.username == username).first()
+
+        session["user_id"] = user.id
+
+        return user.to_dict(), 200
+
+api.add_resource(Login, "/login")
+
+class Logout(Resource):
+    def delete(self):
+        session["user_id"] = None
+        
+        return {"message": "204: No Content"}, 204
+
+api.add_resource(Logout, "/logout")
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get("user_id")).first()
+
+        if user:
+            return user.to_dict(), 200
+        else:
+            return {}, 401
+    
+api.add_resource(CheckSession, "/check_session")
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
